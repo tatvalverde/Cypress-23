52d616c87c69575e1f8af656ea90117ce647553b
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-10-23 22:39:48 -0400

    Add First Name , last Name variable

--- cypress/e2e/practiceForm.cy.js
+++ cypress/e2e/practiceForm.cy.js
@@ -1,10 +1,18 @@
 /// <reference types='cypress' />
 
 describe('Fill out Student Registration Form', ()=> {
+
+  
     it('', ()=> {
       cy.visit('https://demoqa.com/automation-practice-form')
-      cy.get('#firstName').type('Tatiana')
-      cy.get('#lastName').type('Nano').clear().type('Naumova')
+      cy.viewport(1920, 1080)
+
+      cy.get('#firstName').should('have.text', '')
+      cy.get('#firstName').type('Tatiana').should('have.value', 'Tatiana')
+
+      cy.get('#lastName').should('have.text', '')
+      cy.get('#lastName').type('Nano').clear().type('Naumova').should('have.value', 'Naumova')
+
       cy.get('#userEmail').type('test@gmail.com')
       cy.get('#gender-radio-2').check({force: true}).should('be.checked');
       cy.get('#gender-radio-1').should('not.be.checked');
@@ -17,20 +25,21 @@ describe('Fill out Student Registration Form', ()=> {
       cy.get('.react-datepicker__day ').contains('10').click()
       cy.get('#subjectsInput').type('Maths').type('{enter}')
       cy.get('#hobbies-checkbox-1').check({force: true}).should('be.checked');
+
       // cy.get('#uploadPicture').selectFile('C:\Cypress 23\cypress\fixtures\cat.jpg')
       cy.get('#uploadPicture').attachFile('cat.jpg')
       cy.get('#currentAddress').type('555 Woodruff Ln.')
       cy.get('#state').click({force: true})
 
-      cy.get('[id^="react-select-3-option-"]').then($els => {
-      const state = Cypress.$.makeArray($els).filter($el =>$el.innerText == 'NCR')
-      return cy.wrap(state)
-  }).click();
+  //     cy.get('div#state  .css-1hwfws3').then($els => {
+  //     const state = Cypress.$.makeArray($els).filter($el =>$el.innerText == 'NCR')
+  //     return cy.wrap(state)
+  // }).click();
 
-    cy.get('[id^="react-select-4-option-"]').then($els => {
-      const state = Cypress.$.makeArray($els).filter($el =>$el.innerText == 'Noida')
-      return cy.wrap(city)
-  }).click();
-
+  //   cy.get('#stateCity-wrapper > :nth-child(3)').then($els => {
+  //     const state = Cypress.$.makeArray($els).filter($el =>$el.innerText == 'Noida')
+  //     return cy.wrap(city)
+  // }).click()
+      cy.get('button#submit').click();
   })
 })
