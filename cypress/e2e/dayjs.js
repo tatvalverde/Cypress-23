1faa4b2b5e7f7864693e941115924f5b207975c6
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-12-06 12:51:24 -0500

    corrections

--- cypress/e2e/dayjs.js
+++ cypress/e2e/dayjs.js
@@ -5,6 +5,10 @@ const date = dayjs()
 // can use       const date = dayjs('2022-10-01')
 
 console.log(`My date is ${date.add(5, 'day').add(10, 'year').format('YYYY-MM-DD')}`);
+
 // FORMAT can be different 'DD MMMM YYYY'  (11 December 2033)
 // FORMAT can be different 'MMM DD, YYYY'  (Dec 11, 2033)
 // FORMAT can be different 'YYYY.MM.DD'  (2033.12.11)
+
+const [ year, month, day ] = date.add(5, 'day').format('YYYY-MM-DD').split('-');
+console.log(year);
