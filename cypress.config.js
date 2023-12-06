2ed6009d76d6c963964c0a5e68403451f3871d3a
Tatiana Naumova <tatiananaumova5@gmail.com>
2023-12-06 11:24:11 -0500

    Add scripts

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth:1920,
   viewportHeight:1080,
-  
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  defaultCommandTimeout: 10000,
+  experimentalWebKitSupport: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
